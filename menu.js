78dd06138ff1cac440b90ffe3be8acdab25ad251
jhonatandigito <102267615+jhonatandigito@users.noreply.github.com>
2022-07-14 11:28:16 -0300

    vscode/settings.json,aula.css,imag/Aceitamos cart\303\243o de d\303\251bito, cr\303\251dito, refei\303\247\303\243o, Pix e carteiras digitais..png,imag/Footer_Totem 1.png,imag/Logos 1.png,imag/Projeto BKB Code 1.png,imag/Rectangle 8.png,imag/Vector.png,imag/banner-whooper.png,imag/banner.png, imag/bk mix ovomaltine.png,imag/bk22.png,imag/cheddar.png,mag/chicken.png,imag/duplo rodeio.png,imag/shake brownie.png,imag/sorvete.png,index.html, menu.js,pedido.html,produtos.js,  style.css

---
+++ menu.js
@@ -0,0 +1,8 @@
+function adicionar () {
+var  b = document.getElementById('button0')
+b.innerText = 'adicionar'
+}
+function adicionado () {
+  var  b = document.getElementById('button0')
+  b.innerText = 'adicionado'
+}
